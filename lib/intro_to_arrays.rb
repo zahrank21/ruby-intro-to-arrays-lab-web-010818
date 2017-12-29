b1b5923a04a9ace8c332f10648ebe95ae8b87d05
zahrank21 <zahrank@gmail.com>
2017-12-29 17:57:36 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,5 +7,23 @@ def array_with_two_elements
 end
 
 def first_element(arr)
-  arr[1]
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
+
+def last_element_with_array_methods(arr)
+  arr.last
+
+end
+
+def length_of_array(arr)
+  arr.length
+
 end
