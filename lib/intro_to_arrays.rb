7b7c71dc265140f556c837f7060b17d855442856
zahrank21 <zahrank@gmail.com>
2017-12-29 17:59:35 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [1,2]
+end
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
+def first_element_with_array_methods(arr)
+  arr.first
+end
+def last_element_with_array_methods(arr)
+  arr.last
+
+end
+
+def length_of_array(arr)
+  arr.length
+
+end
